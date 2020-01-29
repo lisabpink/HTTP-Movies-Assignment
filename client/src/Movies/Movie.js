1da725044bc07bce5e6c800155d4da3c7acdd310
LISA_COOK <lisabpink@gmail.com>
2020-01-29 16:05:13 -0500

    LDC delete working, but still showing in saved list after deleted, mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -31,6 +31,17 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  handleDelete = (e, props) => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${this.state.movie.id}`)
+      .then(res => {
+        this.props.setMovies(res.data);
+        this.props.history.push("/");
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -42,8 +53,18 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
-        <div className="update-button" onClick={() => this.props.history.push(`/update-movie/${this.state.movie.id}`)}>
-          Update
+        <div>
+          <button
+            className="edit-button"
+            onClick={() =>
+              this.props.history.push(`/update-movie/${this.state.movie.id}`)
+            }
+          >
+            Edit
+          </button>
+          <button className="delete-button" onClick={this.handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
     );
